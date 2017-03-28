47e125d099573181b04606f8d9f6211a6bb210ae
RevolF <929042297@qq.com>
2017-03-28 14:38:01 +0800

    debugging, returns LCS along with ref sequence in list form

--- lcs.py
+++ lcs.py
@@ -16,28 +16,36 @@ class Lcs:
             self.refLong : returns refined long string in list form, '_' stands for deletion
             self.refShort : returns refined short string in list form, '_' stands for deletion
         '''
-        self.longStr=longStr
-        self.shortStr=shortStr
-        self.colNbr=len(longStr)
-        self.rowNbr=len(shortStr)
+        if len(longStr) >= len(shortStr):
+            self.longStr=longStr
+            self.shortStr=shortStr
+        else:
+            self.longStr=shortStr
+            self.shortStr=longStr
+        
+        self.colNbr=len(self.longStr)
+        self.rowNbr=len(self.shortStr)
         self._matrix=''
         self.lcs=[]
         self.refLong=[]
         self.refShort=[]
-        
+        self.lcs=[]
+        self._analysis()
         
     def _initializeMatrix(self):
         '''
         initialize matrix: each element of longStr as index of columns
                 and shortStr for index of rows
+        Cell:
+            lowerEle,upperEle,rowNbr,colNbr,value=0,cellPointer='',prevCell=''
         '''
         cellMatrix=[]
         firstRow=[Cell('NASTR','NASTR',-1,-1)]
         for i in range(self.colNbr):
-            firstRow.append(Cell('NASTR',self.longStr[i],0,i))
+            firstRow.append(Cell('NASTR',self.longStr[i],0,i,i+1))
         cellMatrix.append(firstRow)
         for i in range(self.rowNbr):
-            tmpRow=[Cell(self.shortStr[i],'NASTR',i,-1)]
+            tmpRow=[Cell(self.shortStr[i],'NASTR',i,-1,i+1)]
             for j in range(self.colNbr):
                 tmpRow.append(Cell(self.shortStr[i],self.longStr[j],i,j))
             cellMatrix.append(tmpRow)
@@ -49,9 +57,14 @@ class Lcs:
             for j in range(1,self.colNbr+1):
                 if self._matrix[i][j].lowerEle == self._matrix[i][j].upperEle:
                     self._matrix[i][j].value = self._matrix[i-1][j-1].value
+                    '''
                     self.lcs.append(self._matrix[i][j].lowerEle)
+                     ### note that this would cause false results inside a for loop ###
+                        use a cellPointer for indicating lcs
+                    '''
+                    
                 else:
-                    self._matrix[i][j].value = min(self._matrix[i-1][j],self._matrix[i-1][j-1],self._matrix[i][j-1]) + 1
+                    self._matrix[i][j].value = min(self._matrix[i-1][j].value,self._matrix[i-1][j-1].value,self._matrix[i][j-1].value) + 1
         return
     
     def _backTrace(self):
@@ -59,32 +72,60 @@ class Lcs:
         j=self.colNbr
         curCell=self._matrix[i][j]
         
+        '''
+        when applying backtracing, should place string after condition returns true
+        '''
+        '''
         self.refLong=[self._matrix[i][j].upperEle]
         self.refShort=[self._matrix[i][j].lowerEle]
+        '''
         
-        while i>=0 and j>=0:
+        self.refLong=[]
+        self.refShort=[]
+        '''
+        avoid loop adding string when there's a blanc
+        '''
+        
+        while i>=1 and j>=1:
             cellPointer = self._returnPointer(self._matrix[i-1][j-1],self._matrix[i-1][j],self._matrix[i][j-1])
             if cellPointer == 1:
                 curCell.prevCell=self._matrix[i-1][j-1]
-                self.refLong.insert(0,self._matrix[i-1][j-1].upperEle)
-                self.refShort.insert(0,self._matrix[i-1][j-1].lowerEle)
+#                self.refLong.insert(0,self._matrix[i-1][j-1].upperEle)
+#                self.refShort.insert(0,self._matrix[i-1][j-1].lowerEle)
+                self.refLong.insert(0,self._matrix[i][j].upperEle)
+                self.refShort.insert(0,self._matrix[i][j].lowerEle)
+                if self._matrix[i][j].upperEle == self._matrix[i][j].lowerEle:
+                    self.lcs.insert(0,self._matrix[i][j].upperEle)
                 i -= 1
                 j -= 1
             elif cellPointer == 2:
                 curCell.prevCell=self._matrix[i-1][j]
                 self.refLong.insert(0,'_')
-                self.refShort.insert(0,self._matrix[i-1][j].lowerEle)
+#                self.refShort.insert(0,self._matrix[i-1][j].lowerEle)
+                self.refShort.insert(0,self._matrix[i][j].lowerEle)
                 i -= 1
             elif cellPointer == 3:
                 curCell.prevCell=self._matrix[i][j-1]
-                self.refLong.insert(0,self._matrix[i][j-1].upperEle)
+#                self.refLong.insert(0,self._matrix[i][j-1].upperEle)
+                self.refLong.insert(0,self._matrix[i][j].upperEle)
                 self.refShort.insert(0,'_')
                 j -= 1
             curCell=curCell.prevCell
             
+        if i == 0 and j >= 1:
+            while j >= 1:
+                self.refLong.insert(0,self._matrix[i][j].upperEle)
+                self.refShort.insert(0,'_')
+                j -= 1
+        elif j == 0 and i >= 1:
+            while i >= 1:
+                self.refShort.insert(0,self._matrix[i][j].lowerEle)
+                self.refLong.insert(0,'_')
+                i -= 1
+            
         return
     
-    def _returnPointer(t1,t2,t3):
+    def _returnPointer(self,t1,t2,t3):
         '''
         note that t1,t2,t3 are all Cell objects
             t1 indicates topleft
@@ -94,6 +135,8 @@ class Lcs:
             1 indicates topleft
             2 indicates topright
             3 indicates bottomleft
+        should notice a special case:
+            backtracing has reached the first row or col
         '''
         if t1.value <= t2.value and t1.value <= t3.value:
             return 1
@@ -102,13 +145,19 @@ class Lcs:
         elif t3.value < t1.value and t3.value <= t2.value:
             return 3
         
-    def analysis(self):
+    def _analysis(self):
         self._initializeMatrix()
         self._processMatrix()
         self._backTrace()
-        del self._matrix
+#        del self._matrix
         return
         
+    def getMatrix(self):
+        matrix=[]
+        for i in range(self.rowNbr+1):
+            tmpRow=[a.value for a in self._matrix[i]]
+            matrix.append(tmpRow)
+        return matrix
 
 
 class Cell:
@@ -118,6 +167,7 @@ class Cell:
             1 indicates topleft
             2 indicates topright
             3 indicates bottomleft
+        add a flag in Cell to ensure that each element is added only once
         '''
         self.upperEle=upperEle
         self.lowerEle=lowerEle
